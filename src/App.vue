758becedc10fca158d6029cc42f2cab11135fe24
rosi2509 <79855943+rosi2509@users.noreply.github.com>
2024-04-15 12:10:23 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import BallonDorList from "./components/BallonDorList.vue";
+</script>
+
+<template>
+  <BallonDorList />
+</template>
---
+++ src/components/BallonDorList.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Ballon D'or List</h1>
+  <div class="container">
+    <div
+      v-for="player in players"
+      :key="player.name + player.year"
+      class="player-tile"
+    >
+      <img :src="player.picture" alt="Player Image" />
+      <div class="info">
+        <p>{{ player.name }}</p>
+        <p>Age: {{ player.age }}</p>
+        <p>Year: {{ player.year }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import playersData from "../data/ballonDor.json";
+
+export default {
+  setup() {
+    return {
+      players: playersData.players,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 100px;
+}
+.container {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.container img {
+  width: 200px;
+  height: 200px;
+}
+
+.player-tile {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  background-size: cover;
+  background-position: center;
+}
+
+.player-tile .info {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  padding: 10px;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
